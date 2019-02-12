ec175757a3e2e42983324f41f2027e2c6690dd96
Iosif Balasca <33915089+19manu98@users.noreply.github.com>
2019-02-12 23:02:59 +0000

    Add files via upload
    player moving with platforms

---
+++ Manu_Platforms/PlatformTraverse/Assets/Scripts/InputState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputState : MonoBehaviour
+{
+    public bool actionButton;
+    public float absVelX = 0f;
+    public float absVelY = 0f;
+    public bool standing;
+    public float standingThreshold = 1;
+    // to keep track of the threshold bteween when the velocity is slowing down
+    // and when the player is actually standing
+
+    private Rigidbody2D body2d;
+
+    void Awake()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    // test whether any key has been pressed and set it to the action button
+    void Update()
+    {
+        actionButton = Input.anyKeyDown;
+    }
+
+    void FixedUpdate()
+    {
+        // calculate the absolute value of the velocity.
+        // The player could either be moving up or down, or left or right.
+        // Those values range from positve to negative since the 00 position is
+        // in the middle of the screen in Unity
+
+        absVelX = System.Math.Abs(body2d.velocity.x);
+        absVelY = System.Math.Abs(body2d.velocity.y);
+
+        // test to see if the player is standing or not
+        // if there is no movement on the y-axis, that is considered standing
+        standing = absVelY <= standingThreshold;
+
+    }
+}
+
+/*
+ * This class keeps track of the absolute x and y values of the celocity of the player.
+ * Note when the player is standing, and keep track of any type of input has taken place.
+ */
---
+++ Manu_Platforms/PlatformTraverse/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public void ManipulateTime(float newTime, float duration)
+    {
+        if (Time.timeScale == 0)
+            Time.timeScale = 0.1f;
+
+        StartCoroutine(FadeTo(newTime, duration));
+    }
+        
+
+     IEnumerator FadeTo(float value, float time)
+     {
+         for(float t = 0f; t < 1; t += Time.deltaTime / time)
+         {
+             // this way we increment this based on the difference in time from
+             // one frame to another instead of arbitrarily running this entire loop
+
+             // calculate what the timeScale should be on this current iteration
+             Time.timeScale = Mathf.Lerp(Time.timeScale, value, t);
+
+             if(Mathf.Abs(value - Time.timeScale) < .01f)
+             {
+                 // ensures that we don't have a scenario where time is so close to being 
+                 // near zero that we dont actually reach it or that it takes a very long 
+                 // time to scale down, therefor eif time is close enough to zero we'll set it to zero
+
+                 Time.timeScale = value;
+                 yield return false;
+             }
+
+             yield return null;
+         }
+     }
+}
+
+/*
+ * setting the timeScale to zero when player dies to 
+ * give the affect of pausing the game when the player 
+ * is dead
+ */
