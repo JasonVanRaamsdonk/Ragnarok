c95c5e33887970e818cdbc5c4627584bf135a8b2
Tadhg O'Rourke <orourketadhg@gmail.com>
2019-02-13 20:20:14 +0000

    Initial Commit of Player 2 - Tadhg

---
+++ Tadhg/Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    // movement variables
+    public float CrosshairSpeed;
+    private float VerticalMotion;
+    private float HorizontailMotion;
+    private Rigidbody2D Crosshair;
+
+    // boundary Limits
+    public float maxX, maxY;
+    private float minX, minY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minX = -maxX;
+        minY = -maxY;
+
+        Debug.Log("Player 2 Controller - Active");
+
+        // get crosshair rigidbody component
+        Crosshair = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get input of vertical and horizontal axis
+        VerticalMotion = Input.GetAxis("Vertical");
+        HorizontailMotion = Input.GetAxis("Horizontal");
+
+        // give crosshair a new vector multiplied by CrosshairSpeed
+        Crosshair.velocity = new Vector2(HorizontailMotion * CrosshairSpeed, VerticalMotion * CrosshairSpeed);
+
+        Crosshair.position = new Vector2(Mathf.Clamp(Crosshair.position.x, minX, maxX), Mathf.Clamp(Crosshair.position.y, minY, maxY));
+
+    }
+
+}
