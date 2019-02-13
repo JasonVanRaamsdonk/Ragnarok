8684478eae9497ab683eec18b34f71481486ab16
Vlad Medves <vmedves@gmail.com>
2019-02-13 20:02:59 +0000

    donw

---
+++ Final_Project/Projkt - Kaa/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour, IRecycle
+{
+    public Sprite[] sprites;
+
+    public Vector2 colliderOffset = Vector2.zero;
+
+    public void Restart()
+    {
+        //every time this script is restarted a random sprite will be rendered
+        var renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = sprites[Random.Range(0, sprites.Length)];
+
+        // resizing the collider to dynamically fit each spite
+        //var collider = GetComponent<BoxCollider2D>();
+        //var size = renderer.bounds.size;
+       // size.y += colliderOffset.y;
+
+        //collider.size = size;
+        //collider.offset = new Vector2(-colliderOffset.x, collider.size.y / 2 - colliderOffset.y);
+        // this allows for the manual override any type of offset based
+        // on the type of graphics added
+    }
+
+    public void ShutDown()
+    {
+
+    }
+    
+}
---
+++ Final_Project/Projkt - Kaa/Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour
+{
+    public int textureSize = 32;
+
+    public bool scaleHorizontally = true;
+    public bool scaleVertically = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var newWidth = !scaleHorizontally ? 1 : Mathf.Ceil ( Screen.width / ( textureSize * PixelPerfectCamera.scale));
+        var newHeight = !scaleVertically ? 1 : Mathf.Ceil ( Screen.height / ( textureSize * PixelPerfectCamera.scale));
+        // ternary operator
+
+        // changing scale of quad based on new width & height
+        transform.localScale = new Vector3(newWidth * textureSize, newHeight * textureSize, 1);
+
+        GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
+        //Renderer: What actaully displays the material on the quad
+    }
+
+}
+
+/*
+ * we are trying to find how many tiles will fit 
+ * inside of the screens resolution. We don't want this
+ * value to be lower than the screens resolution (otherwsie we'd
+ * end up with possible games on either side of the screen).
+ * Hence the use of ceil() maths function.
+ * We do this in the Start method and not the Awake method.
+ * If we performed this in the Awake method we could run into the issue 
+ * were the camera hasn't set itself up yet.
+ */
