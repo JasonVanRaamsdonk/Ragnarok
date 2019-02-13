1afcb398b8a90ec461f6a28db631c39771ddb800
unknown <C17764691@mydit.ie>
2019-02-13 19:23:11 +0000

    remove unnecessary folders

---
+++ Jason_Platforms/PlatformTraverse/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float velX = 5f;
+    public float velY = 5f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(velX, velY);
+    }
+}
