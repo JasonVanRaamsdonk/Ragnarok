a4fb16e6856e6264f34af410dc13e789a44d0004
Emanuel Balasca <C17719309@mydit.ie>
2019-02-13 21:03:03 +0000

    Fuck

---
+++ Final_Project/Projkt - Kaa/Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    private BoxCollider2D playCollider;
+
+    private BoxCollider platformCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playCollider = GameObject.find
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
