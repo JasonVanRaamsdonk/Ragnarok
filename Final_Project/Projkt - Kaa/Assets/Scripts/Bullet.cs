ffd8ecad75485fcbc353669aaa30cb3b61d6e420
unknown <C17764691@mydit.ie>
2019-02-18 17:55:50 +0000

    platform damage colour change

--- Final_Project/Projkt - Kaa/Assets/Scripts/Bullet.cs
+++ Final_Project/Projkt - Kaa/Assets/Scripts/Bullet.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damage = 50;
-    public float speed = 20f;
+    //public float speed = 20f;
     public Rigidbody2D rb;
     public GameObject impactEffect;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = transform.right * speed;
+        //rb.velocity = transform.right * speed;
 
     }
 
--- Final_Project/Projkt - Kaa/Assets/Scripts/Spawner.cs
+++ Final_Project/Projkt - Kaa/Assets/Scripts/Spawner.cs
@@ -16,6 +16,7 @@ public class Spawner : MonoBehaviour
     {
         ResetDelay();
         StartCoroutine(EnemyGenerator());
+        
     }
      
     IEnumerator EnemyGenerator()    // to return an interface of Ienumerator
