0dd41cf75dc3d13cd16c2a4197a4a647712945a5
Emanuel Balasca <C17719309@mydit.ie>
2019-02-19 23:22:53 +0000

    best offscreen practice and comments

--- Final_Project/Projkt - Kaa/Assets/Scripts/arrowDistuction.cs
+++ Final_Project/Projkt - Kaa/Assets/Scripts/arrowDistuction.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class arrowDistuction : MonoBehaviour
 {
+    //add the GameObject Variable (fast to call the object)
     public GameObject sprite1;
 
     // Start is called before the first frame update
     void Start()
     {
+        //save the position of the player so you can use it 
         float posX = sprite1.transform.position.x;
+        //the arrow will follow the x position, while the y is fixed
         this.transform.position = new Vector3(posX, 4.5f, 0);
+        //if the player is inside the camera view the arrow is hide
         if (sprite1.transform.position.y <= 6)
         {
             this.GetComponent<SpriteRenderer>().sortingOrder=-2;
@@ -21,19 +25,22 @@ public class arrowDistuction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //update the current position of the player 
         float posX = sprite1.transform.position.x;
+        //change the position of the arrow
         this.transform.position = new Vector3(posX, 4.5f, 0);
 
+        //check if player if on screen
         if (sprite1.transform.position.y <= 6)
         {
-
+            //hide the arrow
             this.transform.position = new Vector3(posX, 4.5f, 0);
             this.GetComponent<SpriteRenderer>().sortingOrder = -2;
-            this.GetComponent<SpriteRenderer>().sortingLayerName = "Background";
 
         }
         else
         {
+            //show the arrow
             this.transform.position = new Vector3(posX, 4.5f, 0);
             this.GetComponent<SpriteRenderer>().sortingOrder = 4;
         }
