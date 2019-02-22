23b78737fe174a41c992d7d88bdf0dfdc8b948a9
Tadhg O'Rourke <orourketadhg@gmail.com>
2019-02-22 13:30:06 +0000

    changed snake tail assets

--- Final_Project/Projkt - Kaa/Assets/Scripts/Tail.cs
+++ Final_Project/Projkt - Kaa/Assets/Scripts/Tail.cs
@@ -7,9 +7,11 @@ public class Tail : MonoBehaviour
     public int DeletePoint;
     public GameObject tempPoint;
     public int StopPoint;
+
     public float dropSpeed;
     public Vector2 Speed;
     public Vector2 ReverseSpeed;
+
     private Rigidbody2D tail;
     private bool flag;
 
@@ -23,46 +25,9 @@ public class Tail : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y > DeletePoint)
-        {
-            Destroy(gameObject);
-        }
-        else if (transform.position.y < StopPoint)
-        {
-            flag = true;
-        }
 
-        if (flag == false && transform.position.y > StopPoint)
-        {
-            tail.velocity = Speed;
-        }
-        else if (flag == true)
-        {
-            tail.velocity = ReverseSpeed;
-        }
 
     }
 
-    // work in progress animation 
-    /*
-    IEnumerator TailMove()
-    {
-        yield return new WaitForSeconds(1.7f);
-
-        if (flag == false && transform.position.y > StopPoint)
-        {
-            tail.velocity = Speed;
-        }
-        else if (flag == true)
-        {
-            tail.velocity = ReverseSpeed;
-        }
-    }
-
-    IEnumerator TailWhip()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, tempPoint.transform.position, dropSpeed);
-        yield return new WaitForSeconds(0.5f);
-    }
-    */
+    
 }
