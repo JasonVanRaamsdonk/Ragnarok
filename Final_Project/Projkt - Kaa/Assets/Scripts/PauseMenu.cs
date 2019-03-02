90cfe03ab054b21bdc2ddef791382215e506370c
mohammedkadiri <mohammedkadiri72@gmail.com>
2019-03-02 01:39:18 +0000

    Implemented the pause feature as well as inter-linking the buttons to load the correct scene

---
+++ Final_Project/Projkt - Kaa/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+    public bool isPaused;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) { 
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                isPaused = true;
+                PauseUI.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
